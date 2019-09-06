21b8f7ef23aa53895da4c5c6111a7eb60ab9317f
Raùl Mazo <raulmazo@gmail.com>
2019-09-06 16:01:35 +0200

    primeros cambios

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <p>
+                Hola mundo
+            </p>
+        </div>
+    )
+  }
+
+export default Header;
