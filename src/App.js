5db811444db7b9b9fbc0d98a214df8e99453c442
Raùl Mazo <raulmazo@gmail.com>
2019-09-06 16:14:03 +0200

    segundo cambio

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, the first example!
         </a>
       </header>
     </div>
